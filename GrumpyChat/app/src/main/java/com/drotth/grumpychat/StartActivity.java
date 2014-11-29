4c5f6018e8a61e656968a8aef4b71a1c1a4a25f0
Andreas Drotth <drotth@hotmail.com>
2014-11-29 01:59:00 +0100

    Succesful login!
    Rearranged the code a bit and made it more modular. Also added a succesful login and registration procedure with Firebase. Also set up own Firebase app: testda401a.firebaseio.com

--- GrumpyChat/app/src/main/java/com/drotth/grumpychat/StartActivity.java
+++ GrumpyChat/app/src/main/java/com/drotth/grumpychat/StartActivity.java
@@ -9,16 +9,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
 public class StartActivity extends Activity {
 
+    //remember to change this to da401a before demo
+    private static final String FIREBASE_URL = "https://testda401a.firebaseio.com";
     private FragmentManager fragmentManager;
     private LoginFragment loginPage;
     private RegisterFragment registerPage;
+    private Firebase firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Firebase.setAndroidContext(this);
+        firebase = new Firebase(FIREBASE_URL);
 
         fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -28,34 +37,24 @@ public class StartActivity extends Activity {
         fragmentTransaction.commit();
     }
 
-    public void login(View view){
+    public void loginClick(View view){
         EditText emailInsert = (EditText) findViewById(R.id.emailFieldLogin);
         String email = emailInsert.getText().toString();
 
         EditText passInsert = (EditText) findViewById(R.id.passwordFieldLogin);
         String password = passInsert.getText().toString();
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Enter both email and password", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Intent mainIntent = new Intent(this, MainActivity.class);
-            mainIntent.putExtra("User", email);
-            mainIntent.putExtra("Password", password);
-            this.startActivity(mainIntent);
-            this.finish();
-        }
+        login(email, password);
     }
 
-    public void register(View view){
+    public void registerClick(View view){
         int id = view.getId();
 
-        if (id == R.id.regBtn1) {
+        if(id == R.id.regBtn1){
             if (registerPage == null || !registerPage.isAdded()){
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 registerPage = new RegisterFragment();
                 fragmentTransaction.replace(R.id.fragmentViewStart, registerPage);
-                //TODO: Possible to stack several "back-presses". Fix!
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 fragmentTransaction.commit();
@@ -68,15 +67,48 @@ public class StartActivity extends Activity {
             EditText passInsert = (EditText) findViewById(R.id.passwordFieldReg);
             String password = passInsert.getText().toString();
 
-            if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Enter both email and password", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.fragmentViewStart, loginPage);
-                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                fragmentTransaction.commit();
-            }
+            register(email, password);
+        }
+    }
+
+    private void login(String email, String password){
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Enter both email and password", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            firebase.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+                @Override
+                public void onAuthenticated(AuthData authData) {
+                    Intent mainIntent = new Intent(StartActivity.this, MainActivity.class);
+                    StartActivity.this.startActivity(mainIntent);
+                    StartActivity.this.finish();
+                }
+                @Override
+                public void onAuthenticationError(FirebaseError error) {
+                    Toast.makeText(getApplicationContext(), "Login unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void register(final String email, final String password){
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Enter both email and password", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            firebase.createUser(email, password, new Firebase.ResultHandler() {
+                @Override
+                public void onSuccess() {
+                    StartActivity.this.login(email, password);
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    Toast.makeText(getApplicationContext(),"Registration unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
